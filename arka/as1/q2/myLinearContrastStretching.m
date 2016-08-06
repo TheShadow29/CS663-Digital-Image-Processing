1a62cd6316efb0941835b887ad03fcab26859609
Arka Sadhu <ark.sadhu2904@gmail.com>
2016-08-07 04:28:34 +0530

    corrected the error in myLinearStretching

--- arka/as1/q2/myLinearContrastStretching.m
+++ arka/as1/q2/myLinearContrastStretching.m
@@ -3,9 +3,9 @@ function [y] = myLinearContrastStretching(x, a, b, c, d)
     x1 = (x > 0) & (x < a);
     y(x1) = b/a * x(x1);
     x2 = (x >= a) & (x<=c);
-    y(x2) = (d - b) * x(x2) / (c -a) + b - (d - b) * a / (c -a);
+    y(x2) = (d-b)/(c-a) * (x(x2) -a) + b;
     x3 = (x > c) & (x <= 255);
-    y(x3) = (255 - d) * x(x3)/(255 - c) + d - (255 - d) * c/(255 - c); 
+    y(x3) = (255-d)/(255-c) * (x(x3) -c) + d;
     y = uint8(y);
     
 end
