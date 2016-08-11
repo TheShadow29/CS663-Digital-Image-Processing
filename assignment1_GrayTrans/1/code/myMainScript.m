517eed68521924aae0609758a37f389c35b8b8f0
Arka Sadhu <ark.sadhu2904@gmail.com>
2016-08-11 16:14:53 +0530

    adding the assignment1 actual folder

---
+++ assignment1_GrayTrans/1/code/myMainScript.m
@@ -0,0 +1,30 @@
+%% MyMainScript
+
+tic;
+%% Your code here
+circles_concentric = imread('../data/circles_concentric.png');
+barbara_small = imread('../data/barbaraSmall.png');
+
+img1 = myShrinkImageByFactorD(circles_concentric, 2);
+img2 = myShrinkImageByFactorD(circles_concentric, 3);
+figure('Name','original image Concentric Circles');
+    imshow(circles_concentric);
+figure('Name','subsampled by d=2 image');
+    imshow(img1);
+    %save('../subsampled_d_2_image', 'img1');
+figure('Name','subsampled by d=3 image');
+    imshow(img2);
+    %save('subsampled by d=3 image', 'img2');
+    
+img3 = myBilinearInterpolation(barbara_small);
+figure('Name', 'Barbara Small');
+    %axis image;
+    imshow(barbara_small);
+figure('Name', 'Bilinear Interpolation');
+    %axis image;
+    imshow(img3);
+    
+img4 = myNearestNeighborInterpolation(barbara_small);
+figure('Name', 'Nearest Neighbors');
+    imshow(img4);
+toc;
